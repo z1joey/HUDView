b7774ff4adbc59f333453c199e6dfac9333b608a
Joey Zhang <joey.zhang@tgitech.net>
2020-12-02 21:58:10 +0800

    init: project

---
+++ HUDView/HUDView/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  HUDView
+//
+//  Created by Yi Zhang on 2020/12/2.
+//  Copyright © 2020 Yi Zhang. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
