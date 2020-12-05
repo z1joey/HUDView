54bc194f5aaa778ebd00129297d749c7ab268be0
Joey Zhang <joey.zhang@tgitech.net>
2020-12-06 01:51:08 +0800

    refactor: HUDView

--- HUDView/HUDView/HUDView.swift
+++ HUDView/HUDView/HUDView.swift
@@ -9,16 +9,25 @@
 
 import UIKit
 
+typealias ButtonActionBlock = (() -> Void)
+
 class HUDView: UIView, NibLoadable {
     @IBOutlet weak var button: UIButton!
 
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    private var buttonActionBlock: ButtonActionBlock?
+
+    init(buttonAction: ButtonActionBlock?) {
+        super.init(frame: CGRect(x: 20, y: 100, width: 300, height: 200))
         loadFromNib(owner: self)
+        self.buttonActionBlock = buttonAction
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    deinit {
+        print("Deint HUD")
     }
 
     @objc func buttonTapped(_ sender: UIButton) {
@@ -26,6 +35,29 @@ class HUDView: UIView, NibLoadable {
     }
 
     @IBAction func buttonAction(_ sender: Any) {
-        print("tap")
+        buttonActionBlock?()
+    }
+
+    func show() {
+        guard let window = UIApplication.shared.windows.filter({$0.isKeyWindow}).first else { return }
+        guard window.subviews.contains(where: { $0 is HUDView}) == false else { return }
+
+        frame.origin.y = -frame.height
+        window.addSubview(self)
+        
+        UIView.animate(withDuration: 1) {
+            self.center = window.center
+        }
+    }
+
+    static func dismiss() {
+        guard let window = UIApplication.shared.windows.filter({$0.isKeyWindow}).first else { return }
+        guard let hudView = window.subviews.first(where: { $0 is HUDView }) else { return }
+
+        UIView.animate(withDuration: 1, animations: {
+            hudView.frame.origin.y = -hudView.frame.height
+        }, completion: { _ in
+            hudView.removeFromSuperview()
+        })
     }
 }
--- HUDView/HUDView/ViewController.swift
+++ HUDView/HUDView/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var hudView: HUDView?
     var shouldShowHUD: Bool = false
 
     override func viewDidLoad() {
@@ -17,23 +16,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        //animateOnWindow()
-        hudView = HUDView.init(frame: CGRect(x: 100, y: -100, width: 100, height: 100))
-
-        HUD.show(hudView)
+    @IBAction func showAction(_ sender: Any) {
+        HUDView(buttonAction: {
+            print("hahaha")
+        }).show()
     }
 
-    func animateOnWindow() {
-        guard hudView == nil else { return }
-
-        let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
-        hudView = HUDView.init(frame: CGRect(x: 100, y: -100, width: 100, height: 100))
-        window?.addSubview(hudView!)
-
-        UIView.animate(withDuration: 1) {
-            self.hudView?.frame.origin.y = 100
-        }
+    @IBAction func dismissAction(_ sender: Any) {
+        HUDView.dismiss()
     }
 }
 
